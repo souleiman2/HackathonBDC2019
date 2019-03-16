b2add65e5d87238730d7195fc117ce6b3449866b
Diego Lopez <lopdie101@gmail.com>
2019-03-16 17:13:44 +0000

    added loading into pickle file

--- python/datamining.py
+++ python/datamining.py
@@ -1,13 +1,25 @@
 import requests
+import pickle
+import os
 
-max_page_number = 1164
-page_number = 1
-url = 'https://hackathon-api.bdc.n360.io/website_statistics?page={}'.format(page_number)
+filename = 'data.pickle'
 
-response = requests.post(url)
 
-data = response.json()
-items = data["Items"]
+if not os.path.isfile('data.pickle'):
+    max_page_number = 1164
+    items = []
+    for page_number in range(max_page_number):
+        print('{}% done'.format(round(100 * (page_number + 1)/max_page_number, 2)))
+        url = 'https://hackathon-api.bdc.n360.io/website_statistics?page={}'.format(page_number)
 
-print(items[0]["new_users"])
+        response = requests.post(url)
 
+        data = response.json()
+        items += data["Items"]
+    with open(filename, 'wb') as f:
+        pickle.dump(items, f)
+else:
+    with open(filename, 'rb') as f:
+        items = pickle.load(f)
+
+print(items[0].keys())
