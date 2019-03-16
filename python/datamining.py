0a1e9d42a906f2a9ba9f8a692779e71ebf7d5b76
Diego Lopez <lopdie101@gmail.com>
2019-03-16 16:56:58 +0000

    updated python file

--- python/datamining.py
+++ python/datamining.py
@@ -1,11 +1,13 @@
 import requests
 
-max_page_number = 547001
+max_page_number = 1164
 page_number = 1
 url = 'https://hackathon-api.bdc.n360.io/website_statistics?page={}'.format(page_number)
 
 response = requests.post(url)
 
 data = response.json()
-for item in data["Items"]:
-    print(item)
+items = data["Items"]
+
+print(items[0]["new_users"])
+
